a888a32b8333bbe4d10e56d0feee46e0d0a0369f
sky770825 <126495061+sky770825@users.noreply.github.com>
2025-11-22 08:35:59 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,9 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // 重要：這裡必須設定為您的 GitHub Repository 名稱，因為您的網址是 .../page/
+  base: '/page/',
+})
